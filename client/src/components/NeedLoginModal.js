6bc7720ecb0dd0a82083a86fe7733ace6069a6f4
ydh94 <dbeogud9411@gmail.com>
2021-10-23 23:37:23 +0900

    ADD : feat/modal

--- client/src/components/NeedLoginModal.js
+++ client/src/components/NeedLoginModal.js
@@ -1,14 +1,15 @@
 import { useHistory } from "react-router";
 
-export default function SignupCompleteModal({ isModalOpen, openModalHandler }) {
+export default function NeedLoginModal({ isModalOpen, openModalHandler }) {
   const history = useHistory();
   return (
     <>
       {isModalOpen ? (
         <div className="modal--backdrop">
           <div className="modal--view">
-            <div>회원가입을 ㅊㅋ</div>
+            <div>로그인이 필요한 기능입니다</div>
             <div className="modal--btnContainer">
+              <button onClick={openModalHandler}>취소</button>
               <button onClick={() => history.replace("/login")}>로그인</button>
             </div>
           </div>
