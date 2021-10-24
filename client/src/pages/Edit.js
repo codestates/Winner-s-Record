536fc4aaf1671a2a13a421f503ba1f6b47a1a668
ydh94 <dbeogud9411@gmail.com>
2021-10-25 02:08:40 +0900

    FIX : feat/edit

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -10,28 +10,30 @@ export default function Edit() {
     setIsModalOpen(!isModalOpen);
   };
 
+  const history = useHistory();
   const [editInfo, setEditInfo] = useState({
-    username: "",
+    nickname: "",
+    oldpassword: "",
     password: "",
     checkPW: "",
   });
 
   const [validation, setValidation] = useState({
-    username: false,
+    nickname: false,
+    checkNickname: false,
+    oldpassword: false,
     password: false,
     checkPW: false,
   });
 
   const [message, setMessage] = useState({
-    username: "이름을 입력해주세요",
+    nickname: "닉네임은 2~7자리 한글로 입력해주세요",
+    oldpassword: "현재 비밀번호를 입력해주세요",
     password: "비밀번호는 8자리 이상, 숫자, 문자, 특수문자가 포함되어야 합니다",
-    checkPW: "비밀번호를 입력해주세요",
+    checkPW: "비밀번호를 확인해주세요",
   });
 
-  const history = useHistory();
-  //   const dispatch = useDispatch();
-
-  function isUsername(asValue) {
+  function isNickname(asValue) {
     var regExp = /^[가-힣]+$/;
     return regExp.test(asValue);
   }
@@ -42,49 +44,79 @@ export default function Edit() {
     return regExp.test(asValue);
   }
 
-  const handleOnblurName = (key) => (e) => {
-    if (!isUsername(editInfo.username)) {
-      setMessage({ ...message, username: "한글만 입력해주세요" });
+  const handleOnblurName = () => {
+    const { nickname } = editInfo;
+    if (!isNickname(nickname)) {
+      setMessage({ ...message, nickname: "한글만 입력해주세요" });
       return;
     }
-    if (editInfo.username.length > 7 || editInfo.username.length < 2) {
-      setMessage({ ...message, username: "2~7자로 입력해주세요" });
+    if (editInfo.nickname.length > 7 || editInfo.nickname.length < 2) {
+      setMessage({ ...message, nickname: "2~7자로 입력해주세요" });
       return;
     }
     axios
-      .post("", { [key]: e.target.value }) //
+      .post("http://localhost:8080/auth/nickname", { nickname })
       .then((res) => {
-        setValidation({ ...validation, username: true });
-        setMessage({ ...message, username: "사용 가능한 닉네임입니다" });
+        setValidation({ ...validation, checkNickname: true });
+        setMessage({ ...message, nickname: "사용 가능한 닉네임입니다" });
       })
       .catch((err) => {
-        setValidation({ ...validation, username: false });
-        setMessage({ ...message, username: "중복된 닉네임입니다" });
+        setValidation({ ...validation, checkNickname: false });
+        setMessage({ ...message, nickname: "중복된 닉네임입니다" });
       });
   };
+
+  const handleOnblurPassword = () => {
+    const { oldpassword } = editInfo;
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        "http://localhost:8080/auth/password",
+        { oldpassword },
+        { headers: { authorization: `Bearer ${token}` } }
+      )
+      .then((res) => {
+        setValidation({ ...validation, oldpassword: true });
+        setMessage({ ...message, oldpassword: "새 비밀번호를 입력해주세요" });
+      })
+      .catch((err) => {
+        setValidation({ ...validation, oldpassword: false });
+        setMessage({ ...message, oldpassword: "비밀번호를 확인해주세요" });
+      });
+  };
+
   const handleInputValue = (key) => (e) => {
     setEditInfo({ ...editInfo, [key]: e.target.value });
   };
 
-  const handleEdit = () => {
-    const { username, password } = editInfo;
+  const handleEditName = () => {
+    const { nickname } = editInfo;
     const token = localStorage.getItem("token");
     axios
       .put(
-        "",
-        { username, password },
+        "http://localhost:8080/auth",
+        { nickname },
         { headers: { authorization: `Bearer ${token}` } }
       )
       .then((res) => {
-        const { userId, userName, accessToken } = res.data.data;
-        localStorage.removeItem("token");
-        localStorage.setItem("token", accessToken);
-        localStorage.setItem("userId", userId);
-        localStorage.setItem("userName", userName);
-        if (token) {
-          //   dispatch(setUserInfo({ token, userId, userName }));
-          //   handleEditInfo();
-        }
+        openModalHandler();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleEditPW = () => {
+    const { password } = editInfo;
+    const token = localStorage.getItem("token");
+    axios
+      .put(
+        "http://localhost:8080/auth",
+        { password },
+        { headers: { authorization: `Bearer ${token}` } }
+      )
+      .then((res) => {
+        openModalHandler();
       })
       .catch((err) => {
         console.log(err);
@@ -92,13 +124,24 @@ export default function Edit() {
   };
 
   const handleKeyPress = (e) => {
-    if (e.type === "keypress" && e.code === "Enter" && isValid) {
-      handleEdit();
+    const type = e.target.placeholder;
+    if (e.type === "keypress" && e.code === "Enter") {
+      if (type === "새 닉네임") {
+        handleOnblurName();
+        if (validation.checkNickname) {
+          handleEditName();
+        }
+      } else {
+        if (
+          validation.oldpassword &&
+          validation.password &&
+          validation.checkPW
+        ) {
+          handleEditPW();
+        }
+      }
     }
   };
-
-  const isValid =
-    validation.username && validation.password && validation.checkPW;
 
   useEffect(() => {
     setMessage({
@@ -115,11 +158,14 @@ export default function Edit() {
           ? editInfo.checkPW === editInfo.password
             ? "비밀번호가 일치합니다"
             : "비밀번호가 불일치합니다"
-          : "비밀번호를 입력해주세요",
+          : "비밀번호를 확인해주세요",
     });
     setValidation({
       ...validation,
-      username: isUsername(editInfo.username) && editInfo.username.length >= 2,
+      nickname:
+        isNickname(editInfo.nickname) &&
+        editInfo.nickname.length >= 2 &&
+        editInfo.nickname.length < 8,
       password: isPassword(editInfo.password),
       checkPW: editInfo.password === editInfo.checkPW,
     });
@@ -128,53 +174,74 @@ export default function Edit() {
   return (
     <div>
       <div className="inputContainer">
-        <input
-          type="username"
-          placeholder="닉네임"
-          onBlur={handleOnblurName("username")}
-          onChange={handleInputValue("username")}
-          onKeyPress={handleKeyPress}
-        />
-        {message.username === "한글만 입력해주세요" ? (
-          <div>{message.username}</div>
-        ) : (
-          <div>{message.username}</div>
-        )}
-        <input
-          type="password"
-          placeholder="새 비밀번호"
-          onChange={handleInputValue("password")}
-          onKeyPress={handleKeyPress}
-        />
-        {message.password ===
-        "비밀번호는 8자리 이상, 숫자, 문자, 특수문자가 포함되어야 합니다" ? (
-          <div>{message.password}</div>
-        ) : message.password === "사용할 수 있는 비밀번호 입니다" ? (
-          <div>{message.password}</div>
-        ) : (
-          <div>{message.password}</div>
-        )}
-        <input
-          type="password"
-          placeholder="새 비밀번호 확인"
-          onChange={handleInputValue("checkPW")}
-          onKeyPress={handleKeyPress}
-        />
-        {message.checkPW === "비밀번호가 불일치합니다" ? (
-          <div>{message.checkPW}</div>
-        ) : (
-          <div>{message.checkPW}</div>
-        )}
+        <div>
+          <input
+            type="nickname"
+            placeholder="새 닉네임"
+            onBlur={handleOnblurName}
+            onChange={handleInputValue("nickname")}
+            onKeyPress={handleKeyPress}
+          />
+          {message.nickname === "한글만 입력해주세요" ? (
+            <div>{message.nickname}</div>
+          ) : (
+            <div>{message.nickname}</div>
+          )}
+          {validation.nickname && validation.checkNickname ? (
+            <button style={{ color: "green" }} onClick={handleEditName}>
+              수정하기
+            </button>
+          ) : (
+            <button>수정하기</button>
+          )}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="현재 비밀번호"
+            onBlur={handleOnblurPassword}
+            onChange={handleInputValue("oldpassword")}
+            onKeyPress={handleKeyPress}
+          />
+          <div>{message.oldpassword}</div>
+          <input
+            type="password"
+            placeholder="새 비밀번호"
+            onChange={handleInputValue("password")}
+            onKeyPress={handleKeyPress}
+          />
+          {message.password ===
+          "비밀번호는 8자리 이상, 숫자, 문자, 특수문자가 포함되어야 합니다" ? (
+            <div>{message.password}</div>
+          ) : message.password === "사용할 수 있는 비밀번호 입니다" ? (
+            <div>{message.password}</div>
+          ) : (
+            <div>{message.password}</div>
+          )}
+          <input
+            type="password"
+            placeholder="새 비밀번호 확인"
+            onChange={handleInputValue("checkPW")}
+            onKeyPress={handleKeyPress}
+          />
+          {message.checkPW === "비밀번호가 불일치합니다" ? (
+            <div>{message.checkPW}</div>
+          ) : (
+            <div>{message.checkPW}</div>
+          )}
+          {validation.oldpassword &&
+          validation.password &&
+          validation.checkPW ? (
+            <button style={{ color: "green" }} onClick={handleEditPW}>
+              수정하기
+            </button>
+          ) : (
+            <button>수정하기</button>
+          )}
+        </div>
       </div>
       <div className="btnContainer">
-        <button onClick={() => history.push("/main")}>돌아가기</button>
-        {isValid ? (
-          <button style={{ color: "green" }} type="submit" onClick={handleEdit}>
-            가입하기
-          </button>
-        ) : (
-          <button onClick={openModalHandler}>가입하기</button> // onclick 옮기기
-        )}
+        <button onClick={() => history.push("/mypage")}>돌아가기</button>
         <EditCompleteModal
           isModalOpen={isModalOpen}
           openModalHandler={openModalHandler}
