933220f60db2e6cbb22b35aa33e66f078bad202c
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-10-24 16:41:37 +0900

    Add feat/dummy -더미데이터 완료

--- server/data/dummy/Users_Posts.js
+++ server/data/dummy/Users_Posts.js
@@ -9,6 +9,61 @@ const Users_Posts = [
     userId: 2,
     postId: 1,
   },
+  {
+    id: 3,
+    userId: 2,
+    postId: 2,
+  },
+  {
+    id: 4,
+    userId: 2,
+    postId: 5,
+  },
+  {
+    id: 5,
+    userId: 2,
+    postId: 6,
+  },
+  {
+    id: 6,
+    userId: 2,
+    postId: 6,
+  },
+  {
+    id: 7,
+    userId: 2,
+    postId: 7,
+  },
+  {
+    id: 8,
+    userId: 5,
+    postId: 3,
+  },
+  {
+    id: 9,
+    userId: 20,
+    postId: 8,
+  },
+  {
+    id: 10,
+    userId: 20,
+    postId: 10,
+  },
+  {
+    id: 11,
+    userId: 20,
+    postId: 3,
+  },
+  {
+    id: 12,
+    userId: 15,
+    postId: 5,
+  },
+  {
+    id: 13,
+    userId: 15,
+    postId: 7,
+  },
 ];
 
 export default Users_Posts;
