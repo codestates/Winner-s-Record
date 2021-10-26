ae1d25b48d54cefa6a265848dcf0aa5b54911954
glen <glen910115@gmail.com>
2021-10-26 11:35:25 +0900

    Fix fix/getOne - token null 시 게스트로

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,6 +9,7 @@ router.post('/', authController.signup);
 router.post('/email', authController.emailValidator);
 router.post('/nickname', authController.nicknameValidator);
 router.post('/login', authController.login);
+router.get('/kakao', authController.kakaoLogin)
 router.post('/logout', jwtValidator, authController.logout);
 router.post('/password', jwtValidator, authController.passwordValidator);
 router.put('/', jwtValidator, authController.edit);
