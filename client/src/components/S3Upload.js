6b277437fa253d27f7d2a3719eccf1f12186d3da
ydh94 <dbeogud9411@gmail.com>
2021-10-28 02:10:41 +0900

    Add feat/mypage - S3 업로드

--- client/src/components/S3Upload.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function S3Upload() {
-  const [progress, setProgress] = useState(0);
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [showAlert, setShowAlert] = useState(false);
-
-  return;
-}
