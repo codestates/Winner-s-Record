846c5ae623d24f5205ce1000d7249e2e79816d4b
ydh94 <dbeogud9411@gmail.com>
2021-10-28 02:09:40 +0900

    Add feat/mypage - S3 이미지 업로드 기능

--- client/src/components/Profile/EditPhotoModal.js
+++ client/src/components/Profile/EditPhotoModal.js
@@ -1,26 +1,67 @@
 import axios from "axios";
+import AWS from "aws-sdk";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUserInfo } from "../../modules/userInfo";
 
 export default function EditPhotoModal({ isModalOpen, openModalHandler }) {
   const dispatch = useDispatch();
+  const [File, setFile] = useState(null);
+  const [img, setImg] = useState(null);
   const [preview, setPreview] = useState(null);
 
-  const imgOnchange = (event) => {
-    const imageFile = event.target.files[0];
+  const imgOnchange = (e) => {
+    const imageFile = e.target.files[0];
+    setFile(imageFile);
     const imageURL = URL.createObjectURL(imageFile);
     setPreview(imageURL);
   };
 
-  // const upload 여기서 업로드하고 그 주소 따온 다음에 포스트 요청 보내기
+  const ACCESS_KEY = "AKIA6LEUKF6HOQL5GBPL";
+  const SECRET_ACCESS_KEY = "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";
+  const REGION = "ap-northeast-2";
+  const S3_BUCKET = "winnersrecordimagestorage";
+  const bucketurl =
+    "https://winnersrecordimagestorage.s3.ap-northeast-2.amazonaws.com";
+
+  AWS.config.update({
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  });
+
+  const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: REGION,
+  });
+
+  const uploadFile = (file) => {
+    const params = {
+      ACL: "public-read",
+      Body: file,
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      ContentType: "image",
+    };
+    myBucket
+      .putObject(params)
+      .on("success", (res) => {
+        const path = res.request.httpRequest.path;
+        setImg(`${bucketurl}${path}`);
+      })
+      .send((err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  };
 
   const handleEdit = () => {
+    uploadFile(File);
     const oldToken = localStorage.getItem("token");
     axios
       .post(
         "http://localhost:8080/auth/img",
-        // { img },
+        { img },
         { headers: { authorization: `Bearer ${oldToken}` } }
       )
       .then((res) => {
@@ -28,6 +69,10 @@ export default function EditPhotoModal({ isModalOpen, openModalHandler }) {
         localStorage.removeItem("token", oldToken);
         localStorage.setItem("token", token);
         dispatch(setUserInfo(userdata));
+        setFile(null);
+        setImg(null);
+        setPreview(null);
+        openModalHandler();
       });
   };
 
---
+++ client/src/components/S3Upload.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function S3Upload() {
+  const [progress, setProgress] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+
+  return;
+}
