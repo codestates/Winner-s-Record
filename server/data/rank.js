4f8cdaa928bede389d2409cc76a159c7ae3b9184
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-10-27 22:17:08 +0900

    Add feat/dbrank - 전체 rank / 닉네임 검색 랭킹 기능 구현

--- server/data/rank.js
+++ server/data/rank.js
@@ -1,44 +1,101 @@
+import pkg from 'sequelize';
 import db from '../models/index.js';
+const { Op } = pkg;
 
-export async function findByEvent(event) {
-  const record = Records.filter((el) => el.event === event);
-  for (let i = 0; i < record.length; i++) {
-    for (let j = 0; j < Users.length; j++) {
-      if (record[i].userId === Users[j].id) {
-        record[i].nickname = Users[j].nickname;
-        record[i].img = Images.find((el) => el.id === Users[j].img).link;
+export async function findAllRank(event) {
+  const recordRank = await db.Records.findAll({
+    where: {
+      event: event,
+    },
+    order: [['point', 'DESC']],
+    limit: 20
+  })
+  const userId = recordRank.map((el) => el.dataValues.userId)
+  const users = await db.Users.findAll({
+    where: {
+      id: {
+        [Op.in]: userId
+      }
+    }
+  })
+  const recordList = recordRank.map((el) => el.dataValues)
+  const userList = users.map((el) => el.dataValues)
+  
+  for(let i = 0; i < recordList.length; i++) {
+    for(let j = 0; j < userList.length; j++) {
+      if(recordList[i].userId === userList[j].id) {
+        recordList[i].nickname = userList[j].nickname
+        recordList[i].img = userList[j].img
       }
     }
   }
-  return record
-    .sort((a, b) =>
-      a.nickname < b.nickname ? -1 : a.nickname > b.nickname ? 1 : 0
-    )
-    .sort((a, b) => b.point - a.point);
+  return recordList
+  .sort((a, b) =>
+    a.nickname < b.nickname ? -1 : a.nickname > b.nickname ? 1 : 0
+  )
+  .sort((a, b) => b.point - a.point);
 }
 
-export async function findByNickname(nickname) {
-  const user = Users.filter((el) => el.nickname.includes(nickname));
-  const userRecord = Records.filter((el) =>
-    user.map((ele) => ele.id).includes(el.userId)
-  );
-  for (let i = 0; i < userRecord.length; i++) {
-    for (let j = 0; j < user.length; j++) {
-      if (userRecord[i].userId === user[j].id) {
-        userRecord[i].nickname = user[j].nickname;
-        userRecord[i].img = Images.find((el) => el.id === Users[j].img).link;
+export async function findNicknameRank(event, nickname) {
+  const recordRank = await db.Records.findAll({
+    where: {
+      event: event,
+    },
+    order: [['point', 'DESC']]
+  })
+  const user = await db.Users.findOne({
+    where: {
+      nickname: nickname
+    }
+  })
+  const recordList = recordRank.map((el) => el.dataValues)
+  const index = recordList.map((el) => el.userId).indexOf(user.dataValues.id)
+
+  let rankList
+  if(index === 0 || index === 1 || index === 2) {
+    rankList = recordList.slice(0, 5)
+  } else if (index >= recordList.length-3) {
+    rankList = recordList.slice(recordList.length - 5, recordList.length)
+  } else {
+    rankList = recordList.slice(index-2, index+3)
+  }
+
+  const userId = rankList.map((el) => el.userId)
+  const users = await db.Users.findAll({
+    where: {
+      id: {
+        [Op.in]: userId
+      }
+    }
+  })
+  const userList = users.map((el) => el.dataValues)
+  for(let i = 0; i < rankList.length; i++) {
+    for(let j = 0; j < userList.length; j++) {
+      if(rankList[i].userId === userList[j].id && rankList[i].userId === user.dataValues.id) {
+        rankList[i].nickname = userList[j].nickname
+        rankList[i].img = userList[j].img
+        rankList[i].rank = index+1
+      } else if(rankList[i].userId === userList[j].id) {
+        rankList[i].nickname = userList[j].nickname
+        rankList[i].img = userList[j].img
       }
     }
   }
-  return userRecord
-    .sort((a, b) =>
-      a.nickname < b.nickname ? -1 : a.nickname > b.nickname ? 1 : 0
-    )
-    .sort((a, b) => b.point - a.point);
+  return rankList.sort((a, b) =>
+  a.nickname < b.nickname ? -1 : a.nickname > b.nickname ? 1 : 0).sort((a, b) => b.point - a.point)
 }
 
 export async function validUser(nickname) {
-  return Users.find((el) => el.nickname === nickname);
+  if(nickname === 'all') {
+    return 'ok'
+  }
+  const result = await db.Users.findOne({
+    where: {
+      nickname: nickname
+    }
+  }).catch((err) => console.log(err))
+
+  return result
 }
 
 export async function validEvent(event) {
