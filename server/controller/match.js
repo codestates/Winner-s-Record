4b33503279f3ece892b399809a31477a918c4853
glen <glen910115@gmail.com>
2021-10-29 08:44:39 +0900

    Fix fix/match - 매치 결과 입력시 완료로 status 변경

--- server/controller/match.js
+++ server/controller/match.js
@@ -1,18 +1,17 @@
-import 'express-async-errors';
-import * as matchData from '../data/match.js';
-import * as recordData from '../data/records.js';
-import * as userData from '../data/auth.js';
+import "express-async-errors";
+import * as matchData from "../data/match.js";
+import * as recordData from "../data/records.js";
+import * as docData from "../data/doc.js";
+import * as userData from "../data/auth.js";
 
 export async function insertResult(req, res) {
   const hostId = req.userId;
   const docId = parseInt(req.params.docId);
-  console.log('hostId : ', hostId, 'docId : ', docId);
   const doc = await matchData.findDocById(docId);
-  console.log('포스트정보 : ', doc);
   if (hostId !== doc.userId) {
-    return res.status(403).json({message: '권한이 없습니다'});
+    return res.status(403).json({ message: "권한이 없습니다" });
   }
-  const {winner, loser} = req.body;
+  const { winner, loser } = req.body;
   const winnerId = await userData.findByNickname(winner);
   const loserId = await userData.findByNickname(loser);
   const type = doc.type;
@@ -24,15 +23,15 @@ export async function insertResult(req, res) {
     loserId: loserId.id,
     docId,
   });
-  console.log('생성된 매치 : ', result);
-  if (doc.type === 'match') {
+  if (doc.type === "match") {
     await recordData.createRecords({
       event,
       winnerId: winnerId.id,
       loserId: loserId.id,
     });
-    return res.status(200).json({result});
-  } else if (doc.type === 'tounarment') {
-    return res.status(200).json({result});
+    await docData.editDoc(docId, { status: "완료" });
+    return res.status(200).json({ result });
+  } else if (doc.type === "tounarment") {
+    return res.status(200).json({ result });
   }
 }
