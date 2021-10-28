02d3cb80ba551da940faabfcf6c1456ef6a776c8
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-10-28 12:24:57 +0900

    Add feat/dblike - 좋아요 추가/삭제, 관심글 목록 기능 추가

--- server/controller/like.js
+++ server/controller/like.js
@@ -6,25 +6,27 @@ import {config} from '../config.js';
 export async function searchLike (req, res) {
   const id = req.params.id
   const ids = await likeData.validId(id)
-  if(ids) {
-    const like = await likeData.findById(id)
-    return res.status(200).send({data: like})
-  } else {
-    return res.status(404).send({message: '해당 포스트가 없습니다'})
+  if(ids !== null) {
+    const docs = await likeData.findById(id)
+    if(docs.length !== 0) {
+      return res.status(200).send({data: docs})
+    } else {
+      return res.status(404).send({message: '해당 포스트가 없습니다'})
+    }
   }
 }
 
 export async function addLike (req, res) {
-  const postId = req.body.postId
+  const docId = req.body.docId
   const authorization = req.headers.authorization
   if(!authorization) {
     return res.status(401).send({message: '로그인이 필요한 기능입니다'})
   } else {
     const token = authorization.split(' ')[1];
     const user = await jwt.verify(token, String(config.jwt.secretKey));
-    const likePost = await likeData.addLikePost(user.id, postId)
-    if(user && likePost) {
-      return res.status(200).send({likeId: likePost.id})
+    const likeDoc = await likeData.addLikeDoc(user.id, docId)
+    if(user && likeDoc) {
+      return res.status(200).send({likeId: likeDoc.id})
     } else {
       return res.status(401).send({message: '로그인이 필요한 기능입니다'})
     }
@@ -32,15 +34,15 @@ export async function addLike (req, res) {
 }
 
 export async function deleteLike (req, res) {
-  const postId = req.body.postId
+  const docId = req.body.docId
   const authorization = req.headers.authorization
   if(!authorization) {
     return res.status(401).send({message: '로그인이 필요한 기능입니다'})
   } else {
     const token = authorization.split(' ')[1];
     const user = await jwt.verify(token, String(config.jwt.secretKey));
-    const deletePost = await likeData.deleteLikePost(user.id, postId)
-    if(user && deletePost) {
+    const deleteDoc = await likeData.deleteLikeDoc(user.id, docId)
+    if(user && deleteDoc === 'ok') {
       return res.sendStatus(204)
     } else {
       return res.status(401).send({message: '로그인이 필요한 기능입니다'})
