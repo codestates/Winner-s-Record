3f6a669b168c2c0990c758a128b9fd6793bb9a4f
glen <glen910115@gmail.com>
2021-11-01 09:14:39 +0900

    Add feat/hostId - 토너먼트결과 조회 시 호스트 id 전달

--- server/controller/tournament.js
+++ server/controller/tournament.js
@@ -1,13 +1,13 @@
 import "express-async-errors";
 import * as tournamentData from "../data/tournament.js";
 import * as matchData from "../data/match.js";
-import * as userData from "../data/auth.js";
-import * as docData from "../data/doc.js";
 
 export async function getAll(req, res) {
   const docId = parseInt(req.params.docId);
+  const doc = await matchData.findDocById(docId);
+  const hostId = doc.userId;
   const result = await tournamentData.findByDocId(docId);
-  return res.status(200).json({ data: result });
+  return res.status(200).json({ data: { ...result, hostId } });
 }
 
 export async function editResult(req, res) {
@@ -22,5 +22,5 @@ export async function editResult(req, res) {
 
   await matchData.eidtMatch(matchId, winner, loser);
   const result = await tournamentData.findByDocId(docId);
-  return res.status(200).json({ data: result });
+  return res.status(200).json({ data: { ...result, hostId: doc.userId } });
 }
