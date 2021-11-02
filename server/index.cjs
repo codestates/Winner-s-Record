d5f42e561a62d6c8e4108f1ef64928a850720511
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-03 00:53:06 +0900

    Add feat/socket - 채팅db저장

--- server/index.cjs
+++ server/index.cjs
@@ -5,9 +5,10 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 require("dotenv").config();
 app.use(cors());
+const db = require("./models/index.js")
 
 const server = http.createServer(app);
-
+console.log(db.Chattings)
 const io = new Server(server, {
   cors: {
     origin: true,
@@ -17,34 +18,39 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join", ({ roomId, nickname }) => {
+  socket.on("join", (data) => {
+    const { roomId, nickname } = data
     socket.join(roomId);
     io.to(roomId).emit("onConnect", {
       content: `${nickname} 님이 입장했습니다.`,
     });
   });
 
-  socket.on("sendMessage", (data) => {
+  socket.on("sendMessage", async (data) => {
     const { message, userId, roomId, time } = data;
-    console.log(data);
     io.to(roomId).emit("receiveMessage", {
       userId,
       message,
       roomId,
       time,
     });
+
+    const chatting = await db.Chattings.create({
+      userId: userId,
+      roomId: roomId,
+      content: message
+    }).catch((err) => console.log(err))
   });
 
   socket.on("socketDisconnect", (data) => {
     const { roomId, nickname } = data;
-    socket.leave(roomId);
-
     io.to(roomId).emit("onDisconnect", {
       content: `${nickname} 님이 퇴장하셨습니다.`,
     });
+    socket.leave(roomId);
   });
 });
 
-server.listen(8081, () => {
-  console.log(`8081 서버 실행`);
+server.listen(process.env.SOCKET_PORT, () => {
+  console.log(`${process.env.SOCKET_PORT} 서버 실행`);
 });
