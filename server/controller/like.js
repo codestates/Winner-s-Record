77bf4093daa730c2802b40c3fdb50e7f9d404a9c
glen <glen910115@gmail.com>
2021-11-03 22:17:15 +0900

    Fix fix/total - api 문서와 동기화 점검

--- server/controller/like.js
+++ server/controller/like.js
@@ -1,6 +1,7 @@
 import "express-async-errors";
 import jwt from "jsonwebtoken";
 import * as likeData from "../data/like.js";
+import * as docData from "../data/doc.js";
 import { config } from "../config.js";
 
 export async function searchLike(req, res) {
@@ -11,8 +12,10 @@ export async function searchLike(req, res) {
     if (docs.length !== 0) {
       return res.status(200).send({ data: docs });
     } else {
-      return res.status(404).send({ message: "해당 포스트가 없습니다" });
+      return res.status(404).send({ message: "해당사항을 찾을 수 없습니다" });
     }
+  } else {
+    return res.status(404).send({ message: "해당사항을 찾을 수 없습니다" });
   }
 }
 
@@ -23,18 +26,25 @@ export async function addLike(req, res) {
     return res.status(401).send({ message: "로그인이 필요한 기능입니다" });
   } else {
     const token = authorization.split(" ")[1];
-    const user = await jwt.verify(token, String(config.jwt.secretKey));
+    let user;
+    try {
+      user = await jwt.verify(token, String(config.jwt.secretKey));
+    } catch {
+      return res.status(401).send({ message: "로그인이 필요한 기능입니다" });
+    }
     const likeDoc = await likeData.addLikeDoc(user.id, docId);
     if (user && likeDoc) {
       return res.status(201).send({ likeId: likeDoc.id });
-    } else {
-      return res.status(401).send({ message: "로그인이 필요한 기능입니다" });
     }
   }
 }
 
 export async function deleteLike(req, res) {
   const docId = req.body.docId;
+  const doc = await docData.findById(docId);
+  if (!doc) {
+    return res.status(404).json({ message: "해당사항을 찾을 수 없습니다" });
+  }
   const authorization = req.headers.authorization;
   if (!authorization) {
     return res.status(401).send({ message: "로그인이 필요한 기능입니다" });
