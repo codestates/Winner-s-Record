7c415a2eac89d451e36393875c4b361e8349faaa
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-04 00:54:36 +0900

    Add feat/socket - 실시간 채팅 기능 구현

--- client/src/components/Chat/ListRoom.js
+++ client/src/components/Chat/ListRoom.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const ListRoom = ({ roomData }) => {
   const { id, img, nickname, content, updatedAt } = roomData;
+  const [isLoading, setIsLoading] = useState(true)
 
   const getDate = () => {
     const date = new Date(updatedAt);
     return `${date.getMonth()}월 ${date.getDate()}일`;
   };
-
+  useEffect(()=>{
+    setIsLoading(false)
+  }, [roomData])
   return (
     <li>
-      <Link to={`chat/${id}`}>
+      {isLoading ? null : <Link to={`/chat/${id}`}>
         <div className="img--container">
           <div className="img">
             <img src={img} alt={nickname} />
@@ -19,12 +22,13 @@ const ListRoom = ({ roomData }) => {
         </div>
         <div className="userinfo--container">
           <div className="username">{nickname}</div>
-          <div className="lastchat">{content}</div>
+          <div className="lastchat">{content.split('tlstjdgnsdbeoguddlwjdgnsdjagPwls|')[1] ? JSON.parse(content.split('tlstjdgnsdbeoguddlwjdgnsdjagPwls|')[1]).title : content}</div>
         </div>
         <div className="date--container">
           <div className="date">{getDate()}</div>
         </div>
-      </Link>
+      </Link>}
+      
     </li>
   );
 };
