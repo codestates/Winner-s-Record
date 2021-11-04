fa2c53389c4c4cf56a52a3259a02d5c222e8f69b
Glen <glen15@naver.com>
2021-11-03 23:12:23 -0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,7 @@ cd /home/ubuntu/Winner-s-Record/server
 export ISIN=$(aws ssm get-parameters --region ap-northeast-2 --names ISIN --query Parameters[0].Value | sed 's/"//g')
 export A=a
 
+pm2 status
 authbind --deep pm2 start app.js
+sleep 10s && pm2 status
 sleep 10s && pm2 status
