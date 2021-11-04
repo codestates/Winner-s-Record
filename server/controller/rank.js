7f6b5f15e140722504463866f6bc7b7d680491f6
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-11-04 14:46:32 +0900

    Fix feat/changerank - 점수가 0인 경우 수정

--- server/controller/rank.js
+++ server/controller/rank.js
@@ -8,9 +8,11 @@ export async function searchRank (req, res) {
     const user = await rankData.validUser(nickname) 
     if(sport && user === 'ok') {
       const eventRecord = await rankData.findAllRank(event)
-      return res.status(200).send({data: eventRecord})
+      const rankList = await rankData.findTopRank(eventRecord)
+      return res.status(200).send({data: rankList})
     } else if(sport && user !== null){
-      const nicknameRecord = await rankData.findNicknameRank(event, nickname)
+      const eventRecord = await rankData.findAllRank(event)
+      const nicknameRecord = await rankData.findNicknameRank(eventRecord, nickname)
       return res.status(200).send({data: nicknameRecord})
     }
     return res.status(404).send({message: '해당 페이지를 찾을 수 없습니다'})
