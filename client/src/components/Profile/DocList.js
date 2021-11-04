a327cdcc9a434067c7ae83ca99dbac522d149136
ydh94 <dbeogud9411@gmail.com>
2021-11-05 00:11:30 +0900

    Add feat/css - fix

--- client/src/components/Profile/DocList.js
+++ client/src/components/Profile/DocList.js
@@ -1,10 +1,12 @@
 import ProfilePostList from "../Profile/MypagePostList";
+import HistoryList from "./HistoryList";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function DocList({ userId, isMypage }) {
   const [list, setList] = useState([]);
   const [currentTab, setCurrentTab] = useState(0);
+  const [history, setHistory] = useState([]);
   const selectTabHandler = (index) => {
     setCurrentTab(index);
   };
@@ -44,8 +46,21 @@ export default function DocList({ userId, isMypage }) {
       });
   };
 
+  const handleHistoryList = () => {
+    axios
+      .get(`http://localhost:8080/match/history?userId=${userId}`)
+      .then((res) => {
+        setList(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setList([]);
+      });
+  };
+
   useEffect(() => {
     handleCreatedList();
+    handleHistoryList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -54,10 +69,12 @@ export default function DocList({ userId, isMypage }) {
         { content: "작성글", func: handleCreatedList },
         { content: "진행중", func: handleProgressList },
         { content: "관심글", func: handleLikeList },
+        { content: "최근 전적", func: handleHistoryList },
       ]
     : [
         { content: "작성글", func: handleCreatedList },
         { content: "진행중", func: handleProgressList },
+        { content: "최근 전적", func: handleHistoryList },
       ];
 
   return (
@@ -82,11 +99,20 @@ export default function DocList({ userId, isMypage }) {
           );
         })}
       </ul>
-      {list.length ? (
-        <ProfilePostList postList={list} />
-      ) : (
-        <div className="profile--nopost">목록이 비었습니다.</div>
-      )}
+      {docTap[currentTab].content !== "최근 전적" ? (
+        list.length ? (
+          <ProfilePostList postList={list} />
+        ) : (
+          <div className="profile--nopost">목록이 비었습니다.</div>
+        )
+      ) : null}
+      {docTap[currentTab].content === "최근 전적" ? (
+        history.length ? (
+          <HistoryList list={history} />
+        ) : (
+          <div>hi</div>
+        )
+      ) : null}
     </>
   );
 }
