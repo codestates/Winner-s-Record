acc4480a9b6210641cbc83608102749bc80b9e37
Glen <glen15@naver.com>
2021-11-03 23:08:21 -0700

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/Winner-s-Record/server
 export INSTOP=instop
-pm2 stop app 2> /dev/null || true
-pm2 delete app 2> /dev/null || true
+pm2 status
+pm2 stop app 
+pm2 delete app 
+pm2 status
