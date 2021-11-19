5fbe79bc3833fabc4ea3f2cd4824c89fa9eee2c0
glen <glen910115@gmail.com>
2021-11-19 16:16:31 +0900

    Refactoring reFactoring/tournament -토너먼트 결과 입력 과정 닉네임이 아닌 Id로변경

--- server/controller/tournament.js
+++ server/controller/tournament.js
@@ -2,6 +2,7 @@ import "express-async-errors";
 import * as tournamentData from "../data/tournament.js";
 import * as matchData from "../data/match.js";
 import * as docData from "../data/doc.js";
+import * as userData from "../data/auth.js";
 
 export async function getAll(req, res) {
   const docId = parseInt(req.params.docId);
@@ -28,8 +29,12 @@ export async function editResult(req, res) {
     return res.status(403).json({ message: "권한이 없습니다" });
   }
   const { winner, loser } = req.body;
+  const winnerData = await userData.findByNickname(winner);
+  const loserData = await userData.findByNickname(loser);
+  const winnerId = winnerData.id;
+  const loserId = loserData.id;
 
-  await matchData.eidtMatch(matchId, winner, loser);
+  await matchData.eidtMatch(matchId, winnerId, loserId);
   const result = await tournamentData.findByDocId(docId);
   return res.status(200).json({ data: result, hostId: doc.userId });
 }
